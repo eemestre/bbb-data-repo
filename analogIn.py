4fdbccd5b2e4a39b0f40e649833cafe70f0082a0
Beagle User <debian@BeagleBone.localdomain>
2024-10-24 04:25:11 +0000

    atualizacao

--- analogIn.py
+++ analogIn.py
@@ -1,29 +1,35 @@
 import Adafruit_BBIO.ADC as ADC
 from time import time, sleep
+import signal
+
+def readData(signum, frame):
+    global a, pin
+    volt = ADC.read(pin) * 1.8
+    a.append(volt)
 
 ADC.setup()
 pin = "P9_37"
 hz = 1800
-old = time()
 a = []
+t = 2
+
+# set signal -> map handler function
+signal.signal(signal.SIGALRM, readData)
+
+print("Comecando!")
+
+# start signal timer after 0.000001 seconds with activation interval of 1/hz
+signal.setitimer(signal.ITIMER_REAL, 0.000001, 1/hz)
+
+sleep(t)
+
+# stop signal timer
+signal.setitimer(signal.ITIMER_REAL, 0, 0.0)
+
+print(len(a))
 
 with open("voltage.csv", "w") as f:
-    f.write("")
+    for v in a:
+        f.write(str(v)+"\n")
 
-while(True):
-    i = 0
-    while(i < 60):
-        now = time()
-        if(now - old >= 1/hz):
-            old = now
-            now = time()
-
-            raw = ADC.read(pin)
-            volt = raw*1.8
-            a.append(volt)
-            i+=1
-    with open('voltage.csv', 'a') as f:
-        for v in a:
-            f.write(str(v)+"\n")
-    a = []
-    sleep(60)
+print("Fim!")
--- plot.py
+++ plot.py
@@ -1,7 +1,19 @@
 from matplotlib import pyplot as plt
 import pandas as pd
 
-data = pd.read_csv("voltage.csv", header=None)
-x = list(range(0, 60, 1))
-plt.plot(x, data)
-plt.savefig('plot.png')
+def plotXLines(source, path, x):
+    data = pd.read_csv(source, header=None)
+    a = list(range(x))
+    plt.plot(a, data[:x])
+    plt.savefig(path)
+    plt.clf
+
+def plotAll(source, path):
+    data = pd.read_csv(source, header=None)
+    a = list(range(len(data.index)))
+    plt.plot(a, data)
+    plt.savefig(path)
+    plt.clf
+
+plotXLines("voltage.csv", "plot1.png", 120)
+plotAll("voltage.csv", "plot2.png")
