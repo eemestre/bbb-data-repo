b245d0e3e7753db96af8b97d585a83e31d9af19c
root <root@BeagleBone.localdomain>
2024-11-19 20:03:33 +0000

    teste

--- analogIn.py
+++ analogIn.py
@@ -1,8 +1,7 @@
 import Adafruit_BBIO.ADC as ADC
 from time import time, sleep
-import signal
 
-def readData(signum, frame):
+def readData():
     global a, pin
     volt = ADC.read(pin) * 1.8
     a.append(volt)
@@ -13,18 +12,13 @@ hz = 2700
 a = []
 t = 2
 
-# set signal -> map handler function
-signal.signal(signal.SIGALRM, readData)
-
 print("Comecando!")
 
-# start signal timer after 0.000001 seconds with activation interval of 1/hz
-signal.setitimer(signal.ITIMER_REAL, 0.000001, 1/hz)
-
-sleep(t)
-
-# stop signal timer
-signal.setitimer(signal.ITIMER_REAL, 0, 0.0)
+while(True):
+    readData()
+    sleep(1/hz)
+    if len(a) >= (hz/60)*2:
+        break
 
 print(len(a))
 
