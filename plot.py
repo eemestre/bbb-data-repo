f0644d29135329ee09f98fa586defb8658e99d5b
root <root@BeagleBone.localdomain>
2024-11-19 20:09:24 +0000

    testee

--- plot.py
+++ plot.py
@@ -15,5 +15,5 @@ def plotAll(source, path):
     plt.savefig(path)
     plt.clf
 
-plotXLines("voltage.csv", "plot1.png", 45)
-plotAll("voltage.csv", "plot2.png")
+plotXLines("voltage.csv", "img1.png", 45)
+plotAll("voltage.csv", "img2.png")
